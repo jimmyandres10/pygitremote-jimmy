71a21199836262248f0d4c6fad1b358b711176ef
jimmyandres10 <54144235+jimmyandres10@users.noreply.github.com>
2019-08-16 23:34:57 -0500

    tercer proyecto

--- main2.py
+++ main2.py
@@ -3,6 +3,7 @@ print ("elija el numero que desee")
 
 num = int(input("dijite un numero: "))
 
+
 if num % 2==0:	
     print("este numero es par")	
 else:	
---
+++ main3.py
@@ -0,0 +1,35 @@
+print("digite numero 1, numero 2 y numero 3 en respectivo orden")
+
+numero_a = int(input("escriba el primer numero"))
+
+numero_b = int(input("escriba el segundo numero"))
+
+numero_c = int(input("escriba el tercer numero"))
+
+if numero_a > numero_b:
+
+    print(f"el numero mayor es: {numero_a}") 
+
+        if numero_a > numero_b:
+
+            print(f"el numero mayor es:  {numero_a}")
+
+        elif:
+
+            print(f"el numero mayor es: {numero_c}")
+
+elif:
+
+    print(f"el numero mayor es: {numero_b}")
+
+        if numero_b > numero_c:
+
+            print(f"el numero mayor es {numero_b}")
+
+        else:
+
+            print(f"el numero mayor es {numero_c}")
+
+
+
+            
